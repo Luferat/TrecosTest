005b53340bcc1f333755ef72efb802594f5fbad8
André Luferat <contato@luferat.net>
2023-02-06 14:26:50 -0300

    Update Validator.java

--- src/main/java/trecos/tools/Validator.java
+++ src/main/java/trecos/tools/Validator.java
@@ -6,14 +6,11 @@ import java.util.regex.Pattern;
 public class Validator {
 
     public boolean isMail(String email) {
-
         String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
         Pattern pattern = Pattern.compile(emailPattern, Pattern.CASE_INSENSITIVE);
-
         if (email == null && email.length() == 0) {
             return false;
         }
-
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
